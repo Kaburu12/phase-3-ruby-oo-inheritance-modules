fb027e6b95ff658a2bef7f3c347e54bad605c77e
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-08-21 18:50:52 +0300

    done

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,24 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
+# require_relative './dance_module'
+# require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+	include FancyDance::InstanceMethods
+	extend FancyDance::ClassMethods
+
+	attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end   
+  end
+
+#   angelina = Dancer.new
+# puts angelina.twirl
+# # => "I'm twirling!"
+# puts angelina.jump
+# # => "Look how high I'm jumping!"
+
+# puts Dancer.metadata
+# # => "This class produces objects that love to dance."
+
